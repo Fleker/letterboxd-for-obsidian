5bcdeffbde43f74092173d7066b3cc5f9e7db85b
Nick <handnf@gmail.com>
2024-04-04 23:53:19 -0400

    Initial commit

--- main.ts
+++ main.ts
@@ -1,85 +1,55 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { normalizePath } from 'obsidian';
+const { XMLParser } = require("fast-xml-parser");
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	username: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	username: 'default'
 }
 
-export default class MyPlugin extends Plugin {
+export default class LetterboxdPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			id: 'letterboxd-sync',
+			name: 'Sync Letterboxd Diary',
+			callback: async () => {
+				if (!this.settings.username) {
+					throw new Error('Cannot get data for blank username')
 				}
-			}
-		});
+				fetch(`https://us-central1-redside-shiner.cloudfunctions.net/proxy?url=https://letterboxd.com/${this.settings.username}/rss/`)
+					.then(res => res.text())
+					.then(async res => {
+						const parser = new XMLParser();
+						let jObj = parser.parse(res);
+						const filename = normalizePath('/Letterboxd Diary.md')
+						const diaryMdArr = jObj.rss.channel.item.map(item => {
+							return `- Gave [${item['letterboxd:memberRating']} stars to ${item['letterboxd:filmTitle']}](${item['link']}) on [[${item['letterboxd:watchedDate']}]]`
+						})
+						const diaryFile = this.app.vault.getFileByPath(filename)
+						if (diaryFile === null) {
+							this.app.vault.create(filename, `${diaryMdArr.join('\n')}`)
+						} else {
+							this.app.vault.process(diaryFile, (data) => {
+								const diaryContentsArr = data.split('\n')
+								const diaryContentsSet = new Set(diaryContentsArr)
+								diaryMdArr.forEach(entry => diaryContentsSet.add(entry))
+								return `${[...diaryContentsSet].join('\n')}`
+							})
+						}
+					})
+			},
+		})
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
 	}
 
 	async loadSettings() {
@@ -91,44 +61,31 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: LetterboxdPlugin;
+	settings: any
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: LetterboxdPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
 		const {containerEl} = this;
+		this.settings = this.plugin.loadData()
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName('Letterboxd Username')
+			.setDesc('This is the username which we will use to fetch Letterboxd entries. It must be public.')
+			.addText((component) => {
+				component.setPlaceholder('myusername')
+				component.setValue(this.plugin.settings.username)
+				component.onChange(async (value) => {
+					this.plugin.settings.username = value
+					await this.plugin.saveSettings()
+				})
+			})
 	}
 }
